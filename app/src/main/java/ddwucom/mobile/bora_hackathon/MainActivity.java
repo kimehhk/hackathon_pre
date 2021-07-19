5d00859395f5ef98e2f283c659ea91c1396c6972
kimehhk <kimehhk@gmail.com>
2021-07-19 13:30:07 +0900

    First Setting

---
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/MainActivity.java
@@ -0,0 +1,14 @@
+package ddwucom.mobile.bora_hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
