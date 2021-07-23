9be0460615bbeaf22c452c38b86b03edf1500193
KwonMinJi00 <alswlghgk@naver.com>
2021-07-24 06:52:39 +0900

    commit

---
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/Constant.java
@@ -0,0 +1,12 @@
+package ddwucom.mobile.bora_hackathon;
+
+public class Constant {
+    private static final String BASE_PATH = "/html/";
+    public static final String CREATE_URL = BASE_PATH + "addEmp.php";
+    public static final String READ = BASE_PATH + "getAllEmp.php";
+    public static final String UPDATE = BASE_PATH + "updateEmp.php";
+    public static final String DELETE = BASE_PATH + "deleteEmp.php";
+
+    public static final String GET_METHOD = "GET";
+    static final String POST_METHOD = "POST";
+}
