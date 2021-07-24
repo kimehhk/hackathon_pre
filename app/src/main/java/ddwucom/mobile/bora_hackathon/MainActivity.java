4f75bf8feb3323830b8b26e3969d8a3e39de8e1f
rnkwg <rnkwg@naver.com>
2021-07-24 22:38:53 +0900

    First commit

--- app/src/main/java/ddwucom/mobile/bora_hackathon/MainActivity.java
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/MainActivity.java
@@ -3,12 +3,22 @@ package ddwucom.mobile.bora_hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private MyAdapter myAdapter;
+    private ListView listView;
+    private ArrayList<Board> movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myAdapter = new MyAdapter(this, R.layout.custom_adapter_view, movieList);
+        listView = (ListView)findViewById(R.id.customListView);
+        listView.setAdapter(myAdapter);
     }
 }
