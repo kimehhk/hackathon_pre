6975cb3df85b91c51999e9c163ca6ea70a1e0aaa
kimehhk <kimehhk@gmail.com>
2021-07-25 13:26:16 +0900

    BoardRequest edit

--- app/src/main/java/ddwucom/mobile/bora_hackathon/BoardRequest.java
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/BoardRequest.java
@@ -2,6 +2,10 @@ package ddwucom.mobile.bora_hackathon;
 
 
 //import com.android.volley.toolbox.StringRequest;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
 import java.util.Map;
 
 public class BoardRequest extends StringRequest {
@@ -9,7 +13,8 @@ public class BoardRequest extends StringRequest {
     final static private String URL = "http://boragame.co.kr/read.php";
     private Map<String, String> map;
 
-    public BoardRequest(String post_id, String title, String context, String date, String user_id, //Response.Listener<String> listener) {
+    public BoardRequest(String post_id, String title, String context, String date, String user_id, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
 
     }
 
