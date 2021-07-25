678199b1393b319f54a51e3066da2d5f892c022e
kimehhk <kimehhk@gmail.com>
2021-07-25 12:32:10 +0900

    version restart pre

---
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/BoardRequest.java
@@ -0,0 +1,16 @@
+package ddwucom.mobile.bora_hackathon;
+
+
+//import com.android.volley.toolbox.StringRequest;
+import java.util.Map;
+
+public class BoardRequest extends StringRequest {
+
+    final static private String URL = "http://boragame.co.kr/read.php";
+    private Map<String, String> map;
+
+    public BoardRequest(String post_id, String title, String context, String date, String user_id, //Response.Listener<String> listener) {
+
+    }
+
+}
