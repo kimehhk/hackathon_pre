0a37d1640d9cc0147218c366109adca3ce08b5ae
rnkwg <rnkwg@naver.com>
2021-07-25 23:13:34 +0900

    Commit

--- app/src/main/java/ddwucom/mobile/bora_hackathon/DetailActivity.java
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AlertDialog;
@@ -12,6 +13,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
+    Board board;
+    EditText date;
+    EditText title;
+    EditText context;
     MyAdapter_comment myAdapter_comment;
     ListView listView;
     ArrayList<Comment> commentList;
@@ -21,7 +26,17 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        board = (Board) getIntent().getSerializableExtra("board");
+
+        date = findViewById(R.id.et_boardDate);
+        title = findViewById(R.id.et_boardTitle1);
+        context = findViewById(R.id.et_boardContext1);
         listView = findViewById(R.id.customListView);
+
+        date.setText(board.getDate());
+        title.setText(board.getTitle());
+        context.setText(board.getContext());
+
         commentList = new ArrayList();
         myAdapter_comment = new MyAdapter_comment(this, R.layout.custom_adapter_view_comment, commentList);
         listView.setAdapter(myAdapter_comment);
@@ -37,7 +52,7 @@ public class DetailActivity extends AppCompatActivity {
                         .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                
+
                             }
                         })
                         .setNegativeButton("취소", null)
