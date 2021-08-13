72fa655d0d69d06b0839b7a7b5c105f66551f189
KwonMinJi00 <alswlghgk@naver.com>
2021-08-14 02:12:20 +0900

    login 수정

--- app/src/main/java/ddwucom/mobile/bora_hackathon/MainActivity.java
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,11 +15,30 @@ public class MainActivity extends AppCompatActivity {
     private MyAdapter_board myAdapterBoard;
     private ListView listView;
     private ArrayList<Board> movieList;
+    private TextView login;
+    private TextView register;
+    private String id;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("user_id");
+        name = intent.getStringExtra("name");
+        if (name != null) {
+            login.setText(name + "님");
+            register.setText("");
+        }
     }
 
     public void onClick(View v) {
@@ -27,11 +47,18 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.button_board:
                 Intent intent = new Intent(this, BoardActivity.class);
+                if (id != null)
+                    intent.putExtra("user_id", id);
                 startActivity(intent);
                 break;
-            case R.id.button_login:
+            case R.id.login:
                 Intent intent1 = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent1);
+                break;
+            case R.id.register:
+                Intent intent2 = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent2);
+                break;
         }
     }
 }
