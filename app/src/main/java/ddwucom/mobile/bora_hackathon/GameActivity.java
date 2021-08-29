f8ba18fe3f19821db64487c5052f4070db40b351
kimehhk <kimehhk@gmail.com>
2021-08-29 16:07:37 +0900

    temp

--- app/src/main/java/ddwucom/mobile/bora_hackathon/GameActivity.java
+++ app/src/main/java/ddwucom/mobile/bora_hackathon/GameActivity.java
@@ -39,7 +39,6 @@ public class GameActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(R.drawable.gamebackground)
                 .into(img);
-                //.override(200, 200);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             soundPool = new SoundPool.Builder().build();
@@ -48,7 +47,6 @@ public class GameActivity extends AppCompatActivity {
         }
 
         soundManager = new SoundManager(this, soundPool);
-
         soundManager.addSound(0, R.raw.click1);
     }
 
